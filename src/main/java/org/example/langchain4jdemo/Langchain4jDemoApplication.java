f3f7b5ba0b7e1093125b7ce44b6d1d359c88db9a
chenyanqiu <931380058@qq.com>
2025-03-17 16:38:56 +0800

    Revert "Replace MyBatis with MyBatis-Plus"

--- src/main/java/org/example/langchain4jdemo/Langchain4jDemoApplication.java
+++ src/main/java/org/example/langchain4jdemo/Langchain4jDemoApplication.java
@@ -1,11 +1,9 @@
 package org.example.langchain4jdemo;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("org.example.langchain4jdemo.mapper")
 public class Langchain4jDemoApplication {
 
     public static void main(String[] args) {
