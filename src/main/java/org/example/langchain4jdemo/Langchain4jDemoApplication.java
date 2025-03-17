e14422960df57e130b0b95d9e50f2d578d6c859b
chendaben <chendabena@gmail.com>
2025-03-17 18:20:07 +0800

    Revert "Fix MyBatis-Plus configuration and dependencies"
    This reverts commit d7cff495c58cc823600603e417ab2ca7339fb9b6.

--- src/main/java/org/example/langchain4jdemo/Langchain4jDemoApplication.java
+++ src/main/java/org/example/langchain4jdemo/Langchain4jDemoApplication.java
@@ -1,12 +1,8 @@
 package org.example.langchain4jdemo;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 
@@ -16,16 +12,6 @@ public class Langchain4jDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Langchain4jDemoApplication.class, args);
-    }
-    
-    /**
-     * 配置MyBatis-Plus分页插件
-     */
-    @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        return interceptor;
     }
 
 }
--- src/main/resources/db/user_table.sql
+++
@@ -1,7 +0,0 @@
--- 创建用户表，包含性别字段
-CREATE TABLE IF NOT EXISTS user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    gender VARCHAR(255),
-    birthday DATE
-);
